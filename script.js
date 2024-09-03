2808d5f1c00576986366d4c9f51de15c49594923
Mohammad Samiullah <mdsamiullah.in@gmail.com>
2024-09-03 13:01:28 +0530

    all files modified

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const toggleSidebar = document.getElementById('toggleSidebar');
-const cutSidebar = document.getElementById('cutSidebar');
-const sideBar = document.getElementById('SideBar');
-
-toggleSidebar.addEventListener('click', () => {
-    sideBar.classList.toggle('open');
-});
-
-cutSidebar.addEventListener('click', () => {
-    sideBar.classList.remove('open');
-});
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('active');
+  });
